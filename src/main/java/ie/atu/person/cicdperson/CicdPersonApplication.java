e89b196bc31f71bcc46e9e1a8691d96afba328cc
conorkeane <g00391846@atu.ie>
2024-01-08 20:48:26 +0000

    Created application

---
+++ src/main/java/ie/atu/person/cicdperson/CicdPersonApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.person.cicdperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdPersonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/person/cicdperson/CicdPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.person.cicdperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
