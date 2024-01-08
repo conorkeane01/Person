8fbb1685590c88647c86b63509ff0ad2a39908c9
conorkeane <g00391846@atu.ie>
2024-01-08 20:57:38 +0000

    Added Person details to person class

--- src/main/java/ie/atu/person/cicdperson/Person.java
+++ src/main/java/ie/atu/person/cicdperson/Person.java
@@ -1,4 +1,43 @@
 package ie.atu.person.cicdperson;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Min(18)
+    private int age;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String employeeId;
+
+    @NotBlank
+    private String position;
+
+    @NotBlank
+    private String department;
+
 }
+
